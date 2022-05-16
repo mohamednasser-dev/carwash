db1d355d672362c60364cbdd65b30a91b67e8990
Islam Shalaby <islamshalby510@gmail.com>
2022-05-16 08:07:23 +0200

    clear add balance

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -10,11 +10,6 @@
                     <div class="col-md-6 col-sm-6 col-12">
                         <h4>{{ __('messages.show_users') }}</h4>
                     </div>
-                    <div class="col-md-6 pl-0 col-sm-6 col-12 text-right">
-                     <a class="btn btn-success mb-2" id="btn_send" data-toggle="modal" data-target="#zoomup_group_Modal" >
-                         {{ __('messages.send_free_balance_for_all_users') }}
-                     </a>
-                    </div>
                 </div>
             </div>
             <div class="widget-content widget-content-area">
@@ -26,9 +21,7 @@
                             <th>{{ __('messages.user_name') }}</th>
                             <th>{{ __('messages.user_phone') }}</th>
                             <th class="text-center">{{ __('messages.block_active') }}</th>
-                            <th class="text-center">{{ __('messages.send_balance') }}</th>
                             <th class="text-center">{{ __('messages.details') }}</th>
-                            <th class="text-center">{{ __('messages.products') }}</th>
                             @if(Auth::user()->update_data)
                                 <th class="text-center">{{ __('messages.edit') }}</th>
                             @endif
@@ -52,23 +45,10 @@
                                         </a>
                                     @endif
                                 </td>
-                                <td>
-                                    <a class="btn btn-warning mb-2 mr-2 btn-rounded" data-user="{{$user->id}}"
-                                       id="btn_send" data-toggle="modal"
-                                       data-target="#zoomupModal">{{ __('messages.send_balance') }}
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                             stroke-linecap="round" stroke-linejoin="round"
-                                             class="feather feather-briefcase">
-                                            <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
-                                            <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-                                        </svg>
-                                    </a>
-                                </td>
+                                
                                 <td class="text-center blue-color"><a href="/admin-panel/users/details/{{ $user->id }}"><i
                                             class="far fa-eye"></i></a></td>
-                                <td class="text-center blue-color"><a href="{{ route('user.products', $user->id) }}"><i
-                                            class="far fa-eye"></i></a></td>
+                                
                                 @if(Auth::user()->update_data)
                                     <td class="text-center blue-color"><a
                                             href="/admin-panel/users/edit/{{ $user->id }}"><i
